9e892e67d7967c7f783870d4d06492b60fc4624b
Danil <58076451+Danil1996@users.noreply.github.com>
2020-08-31 14:35:17 +0300

    Init file with mistake

--- stringVol.2.js
+++ stringVol.2.js
@@ -42,48 +42,51 @@ function splitElementArray(array) {
 }
 
 function arraySplit(array) {
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i]; 
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
     element.forEach((item, index, element) => {
-      if (item === '') {
+      if (item === "") {
         element.splice(index, 1);
         return element;
       }
-      
     });
     continue;
   }
-return array;
+  return array;
 }
-
 
 function parseTree(array) {
   let object = {};
 
-    for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
+    let a = array[i];
+    let b = array[i + 1];
+    if (b == undefined) {
+      b = array[0];
+    }
 
-      const element = array[i];
-      const secondElement = array[i+1];
-      if (secondElement == undefined){
-        return object;
-      }
-      let a = element.length;
-      let b = secondElement.length
-      
-      let maxLenghtArray = Math.max(a, b);
-      
-      for (let j = 0; j < maxLenghtArray; j++) {
-        if(element[j] === secondElement[j]){
-          object[`${element[j]}`] = {};
+    for (let j = 0; j < a.length; j++) {
+      if (a[j] === b[j]) {
+        if (object[a[j]] === a[j]) {
           continue;
-        }else if(element[j] !== secondElement[j]){          
-          object[`${element[j-1]}`] = element[j];
-          object[`${secondElement[j-1]}`] = secondElement[j];
+        } else {
+          object[a[j]] = {};
           continue;
         }
+      } else if (b[j] === undefined) {
+        object[a[j - 1]][a[j]] = {};
+      } else if (a[j].lastIndexOf(".") >= 0) {
+        let fileExtension = a[j].split(".");
+        object[a[j - 1]] = { [fileExtension[0]]: fileExtension[1] };
+      } else if (a[0] !== b[0]) {
+        object[a[j]] = {};
+      } else if (a[j] !== b[j]) {
+        object[a[j - 1]][a[j]] = {};
+        continue;
       }
-     continue;
     }
-    return object;
-    }
-    console.log(parseTree(arraySplit(splitElementArray(list))));
+    continue;
+  }
+  return object;
+}
+console.log(parseTree(arraySplit(splitElementArray(list))));
