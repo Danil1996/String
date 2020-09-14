13f72904d6aa42649b1c14d0a311e427dd0864f5
Danil <58076451+Danil1996@users.noreply.github.com>
2020-09-14 17:59:50 +0300

    Fix fn(delateEmptySpaces)

--- stringVol.2.js
+++ stringVol.2.js
@@ -1,7 +1,15 @@
 // Test2 build directroy tree from "folder" like list of strings
 // Input array:
 
-
+var list = [
+  "/root/user1/file1.txt",
+  "/root/user2/file15.txt",
+  "/root/user1/folder1/file2.txt",
+  "/root/user1/folder1/file3.mp4",
+  "/root/user1/folder1/folder4",
+  "/root/file4.txt",
+  "/var/lib",
+];
 // Expected result easy version:
 // {
 //     root:{
@@ -37,30 +45,23 @@ function composeDirectoryTree(filePaths) {
 
 function apendSinglPath(directoryTree, path) {
   let descriptors = path.split("/");
-  let nonSpacesDescriptors = deleteEmptySpaces(descriptors);
+  let nonSpacesDescriptors = descriptors.filter(deleteEmptySpaces);
   let currentPosition = directoryTree;
   for (const descriptor of nonSpacesDescriptors) {
     const key = retrieveKey(descriptor);
     const value = retrieveValue(descriptor);
     if (key in currentPosition === false) {
       currentPosition[key] = value;
-      // создаем елемент в индекс.штмл
     }
     currentPosition = currentPosition[key];
-    // сохраняем апендед чайлд.
   }
   return directoryTree;
 }
 
 function deleteEmptySpaces(descriptors) {
-  for (const element of descriptors) {
-    if (element === "") {
-      let indexOfElement = descriptors.indexOf(element);
-      descriptors.splice(indexOfElement, 1);
-      return descriptors;
-    }
+ return descriptors !== '';
   }
-}
+
 
 function retrieveKey(descriptor) {
   let arrayOfKeyValuePair = descriptor.split(".");
