89882a5169cec1a128802218dbaa112fefcce398
Danil <58076451+Danil1996@users.noreply.github.com>
2020-09-24 19:40:36 +0300

    Button fix

--- UI/UI.js
+++ UI/UI.js
@@ -9,43 +9,79 @@ var list = [
   "/var/lib",
 ];
 let data = composeDirectoryTree(list);
+
 function objectTraversal(container, directoryTree) {
   container.append(getKey(directoryTree));
 
   function getKey(directoryTree) {
     let ul = document.createElement("ul");
+    ul.style.listStyle = "none";
     for (let key in directoryTree) {
-      let li = document.createElement("li");
-      li.innerHTML = key;
-      ul.append(li);
+      if (typeof directoryTree[key] === "string") {
+        key = `${key}.${directoryTree[key]}`;
+        let li = document.createElement("li");
+        li.innerHTML = key;
+        ul.append(li);
+        console.log(li);
+        console.log(ul);
+        console.log(key);
+      } else {
+        let btn = document.createElement("button");
+        btn.textContent = "-";
+        btn.style.alignItems = "left";
+        btn.className = "button1";
+        let li = document.createElement("li");
+        li.innerHTML = key;
+        ul.append(li);
+        li.prepend(btn);
+        console.log(li);
+        console.log(ul);
+        console.log(key);
+      
+      
       if (typeof directoryTree[key] === "object") {
         let childrenUl = getKey(directoryTree[key]);
         li.append(childrenUl);
       }
     }
+  }
     return ul;
   }
 }
 let container = document.getElementById("container");
-console.log(objectTraversal(container, data));
+objectTraversal(container, data);
 
-// функция для скрытия раскрытия папки
-for (let li of container.querySelectorAll("li")) {
-  let span = document.createElement("span");
-  li.prepend(span);
-  span.append(span.nextSibling);
+container.addEventListener('click', changeBtn)
+
+function changeBtn(e) {
+  if(e.target.textContent === '-'){
+    e.target.textContent = '+';
+    e.target.className = 'button2';
+  } else if (e.target.textContent === '+'){
+    e.target.textContent = '-';
+    e.target.className = 'button1';
+
+  }
 }
 
-container.onclick = function (event) {
-  if (event.target.tagName != "SPAN") {
-    return;
-  }
 
-  let childrenContainer = event.target.parentNode.querySelector("ul");
-  if (!childrenContainer) return;
+// // функция для скрытия раскрытия папки
+// for (let li of container.querySelectorAll("li")) {
+//   let span = document.createElement("span");
+//   li.prepend(span);
+//   span.append(span.nextSibling);
+// }
 
-  childrenContainer.hidden = !childrenContainer.hidden;
-};
+// container.onclick = function (event) {
+//   if (event.target.tagName != "SPAN") {
+//     return;
+//   }
+
+//   let childrenContainer = event.target.parentNode.querySelector("ul");
+//   if (!childrenContainer) return;
+
+//   childrenContainer.hidden = !childrenContainer.hidden;
+// };
 
 // function pictureAssignment(key) {
 // }
